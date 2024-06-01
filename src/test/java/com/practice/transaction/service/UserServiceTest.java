1d3416a32fa65adada6b55ef82b3ec671d9b9369
lkhlkh23 <lkhlkh09@gmail.com>
2024-06-01 16:13:11 +0900

    테스트

--- src/test/java/com/practice/transaction/service/UserServiceTest.java
+++ src/test/java/com/practice/transaction/service/UserServiceTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.practice.transaction.entity.UserInfo;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UserServiceTest {
 
 	@Autowired
